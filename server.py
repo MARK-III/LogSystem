b9792d0e129eea88b084f58876edf729501d76ad
linaro <linaro@cubietruck.(none)>
2015-11-27 16:53:55 +0000

    First API /body/train working

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
 #!flask/bin/python
 from flask import Flask
+from flask import request
 from flask import jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ''' How to test api\n
-    		   http://[ip address]:10080/test/[some input]'''
+    return ''' Refer to https://github.com/MARK-III/LogSystem'''
 
-@app.route('/test/<input>')
-def testapi(input):
-	return jsonify(input=input,
-				   result='Your data is recorded.')
+@app.route('/body/train', methods=['POST','GET'])
+def train():
+    if request.method == 'POST':
+        content = request.get_json(force=True,silent=True)
+        #parsed_content = json.loads(content)
+        return jsonify(content) 
+    else:
+        return jsonify(status="recorded",
+		       name="Dead lift",
+                       weight=30,
+                       created_at="2015-11-27T00:12:55Z")
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host='192.168.0.93',port=10080)
+    app.debug = True
+    app.run(host='192.168.0.93',port=10080)
