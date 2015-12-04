ba62d3ec2aaf112d1f86114a8954b5e09182f5de
linaro <xjq314@gmail.com>
2015-12-04 15:22:30 +0000

    Change database name

--- test.sql
+++ test.sql
@@ -1,26 +1,33 @@
 DROP TABLE main;
+
 CREATE TABLE main (
 record_id varchar(36),
 exercise_id varchar(30),
 catalog_id varchar(30),
 resistance integer,
 repetition integer,
-group integer,
+groups integer,
 date varchar(10),
 created_at varchar(50),
 );
+
 DROP TABLE exercise;
+
 CREATE TABLE exercise (
 uuid varchar(36),
 name varchar(50),
 catalog_id varchar(36)
 );
+
 DROP TABLE catalog;
+
 CREATE TABLE catalog (
 catalog_id varchar(36),
 name varchar(50),
 );
+
 DROP TABLE user;
+
 CREATE TABLE user (
 user_id varchar(36),
 name varchar(50),
