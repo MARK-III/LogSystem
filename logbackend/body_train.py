969685dac0a46252d159f69b8ca84c73dc423989
linaro <xjq314@gmail.com>
2015-12-07 17:29:57 +0000

    Add body_train

---
+++ logbackend/body_train.py
@@ -0,0 +1,30 @@
+from flask import request
+from flask import jsonify
+from flask import json
+from datetime import datetime
+from uuid import uuid1
+import sqlite3
+
+def getResponse(request):
+
+    body = request.get_json(force = True, silent = True)
+    date = body['date']
+    time = datetime.now()
+    for record in body['records']:
+	
+	for i in range(0, record['groups']):
+
+            exercise = record['exercise']
+	    catalog = record['catalog']
+	    resistance = record['resistance']
+	    repitation = record['repitation']
+	    uid = str(uuid1())
+	
+	    data = (uid,exercise,catalog,resistance,repitation,date,time)
+	    connection = sqlite3.connect('test.db')
+	    cur = connection.cursor()
+	    cur.execute('INSERT INTO main VALUES(?,?,?,?,?,?,?)',data)
+    	    connection.commit()
+    	    connection.close()
+	
+    return str(exercise)
