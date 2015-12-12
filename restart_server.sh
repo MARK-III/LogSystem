7de3a4698f76537ef7c327abf396820b6cd1cb6a
linaro <xjq314@gmail.com>
2015-12-12 14:15:57 +0000

    change name from server to logserver

--- restart_server.sh
+++ restart_server.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-for pid in $(ps aux|grep linaro |grep server.py | awk '{print $2}')
+for pid in $(ps aux|grep linaro |grep logserver.py | awk '{print $2}')
 do
 kill $pid
 done
-nohup python server.py &
+nohup python logserver.py &
