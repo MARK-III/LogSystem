ff61b22c3fdbdb509ebe6d4c45cd7bcd6776bca5
linaro <xjq314@gmail.com>
2016-03-09 00:53:32 +0900

    change package name from logbackend to log

--- logserver.py
+++ logserver.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from flask import redirect
 from flask import url_for
 import json
-import logbackend
+import log
 
 app = Flask(__name__)
 
@@ -15,23 +15,23 @@ def index():
 
 @app.route('/body/train', methods=['POST'])
 def post_train():
-    return logbackend.body_train.getResponse(request)
+    return log.body_train.getResponse(request)
 
 @app.route('/body/records/<date>', methods=['GET'])
 def get_records(date):
-    return logbackend.body_records.getResponse(request,date)
+    return log.body_records.getResponse(request,date)
 
 @app.route('/body/calender/<date>', methods=['GET'])
 def get_calender(date):
-    return logbackend.body_calender.getResponse(request,date)
+    return log.body_calender.getResponse(request,date)
 
 @app.route('/body/catalog', methods=['GET'])
 def get_catalog():
-    return logbackend.body_catalog.getResponse(request)
+    return log.body_catalog.getResponse(request)
 
 @app.route('/body/record/<uuid>', methods=['DELETE'])
 def delete_record(uuid):
-    return logbackend.body_record.getResponse(request,uuid)
+    return log.body_record.getResponse(request,uuid)
 
 if __name__ == '__main__':
     app.debug = True
